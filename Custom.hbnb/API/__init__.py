c0560dc67e9a403a56bc8b6d41c09c11b02030ef
11B-SHRINK <151459682+SPCDIAZRIVERACHRISTIAN@users.noreply.github.com>
2024-06-11 23:54:51 -0400

    Delete Custom.hbnb/API directory

--- Custom.hbnb/API/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask # type: ignore
-from .user_routes import setup_routes
-import .amenities_routes
-
-
-def create_app():
-    app = Flask(__name__)
-    setup_routes(app)
-    return(app)
