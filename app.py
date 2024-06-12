ec18cc3e4e852f9e221dcc8c9a1d24236ec799a3
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-06-11 21:11:40 -0400

    reverted app.py code

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-from api import app, amenities, places, user_routes, country_city_routes
-from flask import Flask
+from api import app
 
-app = Flask(__name__)
-
-app.register_blueprint(user_routes)
 if __name__ == '__main__':
     app.run(debug=True)
